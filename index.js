4d458cd44ed590cb74ba99d7bce4182e6819402d
Doğa Güneş <dogagunes0702@gmail.com>
2025-02-01 15:16:33 +0300

    if else statements added for uv section

--- index.js
+++ index.js
@@ -34,9 +34,25 @@ app.post("/get-uv", async (req,res) => {
         const lat = geocodeResponse.data.coord.lat;
         const lon = geocodeResponse.data.coord.lon
 
-        const response = await axios.get(``);
+        const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${yourAPIKey}`);
+        const result = response.data;
+        const uvScale = result.data[0].uvi;
+        let sentText = "";
+        if(uvScale < 2){
+            sentText = "Low risk. No sun protection needed.😌";
+        }else if(uvScale >= 2 && uvScale < 5){
+            sentText = "Moderate risk. Wear a hat, sunglasses, and apply sunscreen.😎";
+        }else if(uvScale >= 5 && uvScale < 7){
+            sentText = "High risk. Stay in the shade between 11 AM - 4 PM and use SPF 30+ sunscreen.🌤️";
+        }else if(uvScale >= 7 && uvScale < 10){
+            sentText = "Very high risk. Avoid sun exposure as much as possible and wear long-sleeved clothing.🌞";
+        }else if(uvScale >= 10){
+            sentText = "Extreme risk. Sun exposure is very dangerous! Use SPF 50+ and stay out of direct sunlight.🔥";
+        }   
+
         res.render("uv.ejs", {
-            uv: result,
+            text: sentText,
+            city: city,
         });
     }catch(error){
         console.log(error);
@@ -55,7 +71,7 @@ app.post("/get-pollution", async (req,res) => {
         const result = response.data;
         const component = result.list[0].components.so2;
         let sentText = "";
-        if(component >= 0 && component < 20){
+        if(component < 20){
             sentText = "The air quality is Good. It's safe to go outside.😌";
         }else if(component >= 20 && component < 80){
             sentText = "The air quality is Fair. Be cautious if you go outside.😕";
